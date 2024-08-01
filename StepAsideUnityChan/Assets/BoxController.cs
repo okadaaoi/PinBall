a881089c2891cdec45d6f28ec836e1abd075b1ad
okadaaoi <okada@ichica.co.jp>
2024-08-01 19:07:08 +0900

    StepAsideUnityChan

---
+++ StepAsideUnityChan/Assets/BoxController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour
+{
+    //Unityちゃんのオブジェクト
+    private GameObject unitychan;
+    //Unityちゃんとカメラの距離
+    private float difference;
+    private GameObject DestroyContoller;
+    public PrefabDestroy prefabDestroy;
+
+    void Start()
+    {
+        //Unityちゃんのオブジェクトを取得
+        this.unitychan = GameObject.Find("unitychan");
+        //Unityちゃんとカメラの位置（z座標）の差を求める
+        this.difference = unitychan.transform.position.z - this.transform.position.z;
+    }
+
+    void Update()
+    {
+        //Unityちゃんの位置に合わせてカメラの位置を移動
+        this.transform.position = new Vector3(0, this.transform.position.y, this.unitychan.transform.position.z - difference);
+    }
+
+    public void OnColliderEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        PrefabDestroy script = prefabDestroy.GetComponent<PrefabDestroy>();
+    }
+}
